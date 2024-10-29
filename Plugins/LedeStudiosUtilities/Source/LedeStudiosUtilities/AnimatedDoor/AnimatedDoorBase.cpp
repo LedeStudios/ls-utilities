da03ab217d6e284e711b5839b3def3d72a252109
lede-dev <lede.7716@gmail.com>
2024-10-29 23:47:31 +0900

    feat: add animated door classes

---
+++ Plugins/LedeStudiosUtilities/Source/LedeStudiosUtilities/AnimatedDoor/AnimatedDoorBase.cpp
@@ -0,0 +1,111 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AnimatedDoorBase.h"
+
+
+AAnimatedDoorBase::AAnimatedDoorBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Door = CreateDefaultSubobject<USkeletalMeshComponent>("Door");
+	SetRootComponent(Door);
+}
+
+void AAnimatedDoorBase::BeginPlay()
+{
+	Super::BeginPlay();
+
+	UAnimInstance* Anim = Door->GetAnimInstance();
+	checkf(Anim, TEXT("AAnimatedDoorBase::BeginPlay: The Animation Instance for the Door actor must be registered."));
+
+	Anim->OnMontageStarted.AddDynamic(this, &AAnimatedDoorBase::OnAnimationStarted);
+	Anim->OnMontageEnded.AddDynamic(this, &AAnimatedDoorBase::OnAnimationEnded);
+	
+	// Set Door is Opened
+	if (bIsOpened)
+	{
+		Door->SetAnimation(OpenAnimation);
+		Door->SetPosition(1.f, false);
+	}
+}
+
+void AAnimatedDoorBase::OpenDoor() const
+{
+	if (bIsWorking)
+	{
+		return;
+	}
+	
+	// Play animation when door is closed
+	if (!bIsOpened)
+	{
+		if (OpenAnimation)
+		{
+			Door->Stop();
+			Door->PlayAnimation(OpenAnimation, false);
+		}
+	}
+}
+
+void AAnimatedDoorBase::CloseDoor() const
+{
+	if (bIsWorking)
+	{
+		return;
+	}
+	
+	// Play animation when door is opened
+	if (bIsOpened)
+	{
+		if (CloseAnimation)
+		{
+			Door->Stop();
+			Door->PlayAnimation(CloseAnimation, false);
+		}
+	}
+}
+
+bool AAnimatedDoorBase::IsOpened() const
+{
+	return bIsOpened;
+}
+
+bool AAnimatedDoorBase::IsWorking() const
+{
+	return bIsWorking;
+}
+
+void AAnimatedDoorBase::SetProgress(float InProgress) const
+{
+	InProgress = FMath::Clamp(InProgress, 0.f, 1.f);
+	Door->SetPosition(InProgress);
+}
+
+void AAnimatedDoorBase::OnAnimationStarted(UAnimMontage* Montage)
+{
+	bIsWorking = true;
+}
+
+void AAnimatedDoorBase::OnAnimationEnded(UAnimMontage* Montage, bool bInterrupted)
+{
+	bIsWorking = false;
+
+	// Close Door
+	if (bIsOpened)
+	{
+		bIsOpened = false;
+		if (OnDoorClosed.IsBound())
+		{
+			OnDoorClosed.Broadcast();
+		}
+		return;
+	}
+
+	// Open Door
+	bIsOpened = true;
+	if (OnDoorOpened.IsBound())
+	{
+		OnDoorClosed.Broadcast();
+	}
+}
---
+++ Plugins/LedeStudiosUtilities/Source/LedeStudiosUtilities/AnimatedDoor/AnimatedDoorBase.h
@@ -0,0 +1,68 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AnimatedDoorBase.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnDoorDelegate);
+
+UCLASS()
+class LEDESTUDIOSUTILITIES_API AAnimatedDoorBase : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AAnimatedDoorBase();
+
+protected:
+	virtual void BeginPlay() override;
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="AnimatedDoor")
+	TObjectPtr<USkeletalMeshComponent> Door;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="AnimatedDoor")
+	TObjectPtr<UAnimSequence> OpenAnimation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="AnimatedDoor")
+	TObjectPtr<UAnimSequence> CloseAnimation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="AnimatedDoor")
+	bool bIsOpened = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="AnimatedDoor")
+	bool bIsWorking = false;
+	
+public:
+	UFUNCTION(BlueprintCallable, Category="AnimatedDoor")
+	void OpenDoor() const;
+
+	UFUNCTION(BlueprintCallable, Category="AnimatedDoor")
+	void CloseDoor() const;
+
+	UFUNCTION(BlueprintCallable, Category="AnimatedDoor")
+	bool IsOpened() const;
+
+	UFUNCTION(BlueprintCallable, Category="AnimatedDoor")
+	bool IsWorking() const;
+	
+	UFUNCTION(BlueprintCallable, Category="AnimatedDoor")
+	void SetProgress(float InProgress) const;
+
+public:
+	UPROPERTY(BlueprintAssignable, Category="AnimatedDoor")
+	FOnDoorDelegate OnDoorOpened;
+
+	UPROPERTY(BlueprintAssignable, Category="AnimatedDoor")
+	FOnDoorDelegate OnDoorClosed;
+
+private:
+	UFUNCTION()
+	void OnAnimationStarted(UAnimMontage* Montage);
+	
+	UFUNCTION()
+	void OnAnimationEnded(UAnimMontage* Montage, bool bInterrupted);
+	
+};
